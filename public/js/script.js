2046ba1fc8eaa778eb177e5e91e4858004c268c8
Ray Meibaum <rmeibaum@me.com>
2017-09-26 09:07:36 -0400

    stuck on CORS

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,22 @@
+'use-strict'
+
+function handleResults(response) {
+  console.log("handle results called", response)
+}
+
+function submitForm(event) {
+  event.preventDefault()
+  const station = $(this).serializeArray()[0].value
+
+  $.ajax({
+    url: `http://developer.itsmarta.com/NextTrainService/RestServiceNextTrain.svc/GetNextTrain/${station}`,
+    dataType: "jsonp",
+    jsonpCallback: "handleResults"
+  }).done(function(response) {
+    console.log(response)
+  })
+}
+
 $(function() {
-  console.log('jquery')
+  $('form').submit(submitForm)
 })
