30af4ff98a02eb71682feeb7a8cb838283ab2c41
Ray Meibaum <rmeibaum@me.com>
2017-09-28 09:02:08 -0400

    considering switch to vue... 🤔

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,5 @@
 'use-strict'
 
-function jsonCallback(response) {
-  console.log("handle results called", response)
-}
-
 function addStationsToDropdown(response) {
   const $stationSelect = $('#station-select')
   $stationSelect.empty()
@@ -14,18 +10,50 @@ function getStationList() {
   $.get('/api/stations').then(addStationsToDropdown)
 }
 
+function sortTrainsByDirection(trains) {
+  const sortedTrains = {
+    northbound: [],
+    eastbound: [],
+    southbound: [],
+    westbound: [],
+  }
+  trains.forEach(train => {
+    switch (train.DIRECTION) {
+      case "N":
+        sortedTrains.northbound.push(train);
+        break;
+      case "E":
+        sortedTrains.eastbound.push(train);
+        break;
+      case "S":
+        sortedTrains.southbound.push(train);
+        break;
+      case "W":
+        sortedTrains.westbound.push(train);
+        break;
+    }
+  })
+  return sortedTrains
+}
+
 function displayResults(response) {
-  const trainList = JSON.parse(response)
-  const $body = $('body')
-  console.log(trainList)
-  trainList.forEach(train => $body.append(train.DIRECTION))
+  const trains = sortTrainsByDirection(JSON.parse(response))
+  console.log(trains)
+
+}
+
+function updateTitle(stationName) {
+  $('#station-title').html(stationName)
 }
 
 function submitForm(event) {
   event.preventDefault()
   const station = $(this).serializeArray()[0].value
-
-  $.get(`/api/stations/${station}`).then(displayResults)
+  $.get(`/api/stations/${station}`)
+    .then(response => {
+      displayResults(response)
+      updateTitle(station)
+    })
 }
 
 $(function () {
